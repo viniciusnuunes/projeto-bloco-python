b7a6f9bfb6bd3a26428711894c7ad380f0083bc8
Renato Machado <renatofm1989@gmail.com>
2021-05-30 20:56:41 -0300

    barra de uso, porcentagem e espaço livre de uso adicionados

--- disco.py
+++ disco.py
@@ -38,17 +38,48 @@ def __desenha_uso_hd(surface, tela, font):
     text = font.render(texto_barra, 1, CONSTAT.BRANCO)
     tela.blit(text, (20, 10))
 
+    # Desenhando o total de disco
+    largura = CONSTAT.LARGURA_TELA - 2 * 20
+    pygame.draw.rect(surface, CONSTAT.VERMELHO, (20, 50, largura, 30))
+    tela.blit(surface, (0, 100))
+
+    # Desenhando a barra de uso por cima do total de disco
+    largura = largura * disco_info.percent / 100
+    pygame.draw.rect(surface, CONSTAT.AZUL, (20, 50, largura, 30))
+    tela.blit(surface, (0, 100))
+
     # Total de disco
     texto_total = "Total de Disco: (" + str(round(disk_total/(1024*1024*1024), 2))+" GB):"
     text = font.render(texto_total, 1, CONSTAT.BRANCO)
-    tela.blit(text, (20, 150))
+    tela.blit(text, (20, 100))
+
+     # Desenhando o uso de disco
+    largura = CONSTAT.LARGURA_TELA - 2 * 20
+    pygame.draw.rect(surface, CONSTAT.VERMELHO, (20, 50, largura, 30))
+    tela.blit(surface, (0, 200))
+
+    # Desenhando a barra de uso por cima do uso de disco
+    largura = largura * disco_info.percent / 100
+    pygame.draw.rect(surface, CONSTAT.AZUL, (20, 50, largura, 30))
+    tela.blit(surface, (0, 200))
 
     # Uso de disco
     texto_uso = "Uso de Disco: (" + str(round(disk_uso/(1024*1024*1024), 2))+" GB):"
     text = font.render(texto_uso, 1, CONSTAT.BRANCO)
     tela.blit(text, (20, 200))
 
+
+     # Desenhando o espaço livre de disco
+    largura = CONSTAT.LARGURA_TELA - 2 * 20
+    pygame.draw.rect(surface, CONSTAT.VERMELHO, (20, 50, largura, 30))
+    tela.blit(surface, (0, 300))
+
+    # Desenhando a barra de uso por cima do espaço livre de disco
+    largura = largura * disco_info.percent / 100
+    pygame.draw.rect(surface, CONSTAT.AZUL, (20, 50, largura, 30))
+    tela.blit(surface, (0, 300))
+
     # Espaço livre do disco
     texto_livre = "Espaço livre no Disco: (" + str(round(disk_livre/(1024*1024*1024), 2))+" GB):"
     text = font.render(texto_livre, 1, CONSTAT.BRANCO)
-    tela.blit(text, (20, 250))
+    tela.blit(text, (20, 300))
