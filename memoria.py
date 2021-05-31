6a1270fc9a3d9864f6c456c41b6f0f10b0f40722
Renato Machado <renatofm1989@gmail.com>
2021-05-30 21:16:49 -0300

    separacao da porcentagem de uso da memoria e a barra de seu uso

--- memoria.py
+++ memoria.py
@@ -36,37 +36,51 @@ def __desenha_memoria(surface, tela, font):
     total = round(memoria_info.total/(1024*1024*1024), 2)
 
     # Desenhando o texto acima da barra de memória
-    texto_barra = "Uso de Memória (Total: " + str(total) + \
-        "GB) (Utilizando: " + str(memoria_info.percent) + " %):"
+    texto_barra = "Uso de Memória (Total: " + str(total) + " GB):"
     text = font.render(texto_barra, 1, CONSTAT.BRANCO)
     tela.blit(text, (20, 10))
+
+    # Desenhando a porcentagem de uso da memoria
+    largura = CONSTAT.LARGURA_TELA - 2 * 20
+    pygame.draw.rect(surface, CONSTAT.VERMELHO, (20, 50, largura, 30))
+    tela.blit(surface, (0, 100))
+
+    # Desenhando a barra da porcentagem de uso da memoria
+    largura = largura * memoria_info.percent / 100
+    pygame.draw.rect(surface, CONSTAT.AZUL, (20, 50, largura, 30))
+    tela.blit(surface, (0, 100))
+
+    # Total da porcentagem da memoria em uso
+    texto_total = "Porcentagem de memória utilizado (" + str(memoria_info.percent) + "%):"
+    text = font.render(texto_total, 1, CONSTAT.BRANCO)
+    tela.blit(text, (20, 100))
 
     # Desenhando o total de memoria em uso
     largura = CONSTAT.LARGURA_TELA - 2 * 20
     pygame.draw.rect(surface, CONSTAT.VERMELHO, (20, 50, largura, 30))
-    tela.blit(surface, (0, 100))
+    tela.blit(surface, (0, 200))
 
     # Desenhando a barra de uso por cima do total de memoria em uso
     largura = largura * memoria_info.percent / 100
     pygame.draw.rect(surface, CONSTAT.AZUL, (20, 50, largura, 30))
-    tela.blit(surface, (0, 100))
+    tela.blit(surface, (0, 200))
 
     # Total de memoria em uso
     texto_total = "Total de Memória: (" + str(round(mem_uso/(1024*1024*1024), 2))+" GB):"
     text = font.render(texto_total, 1, CONSTAT.BRANCO)
-    tela.blit(text, (20, 100))
+    tela.blit(text, (20, 200))
 
     # Desenhando o total de memoria livre
     largura = CONSTAT.LARGURA_TELA - 2 * 20
     pygame.draw.rect(surface, CONSTAT.VERMELHO, (20, 50, largura, 30))
-    tela.blit(surface, (0, 200))
+    tela.blit(surface, (0, 300))
 
     # Desenhando a barra de uso por cima do total de memoria livre
     largura = largura * memoria_info.percent / 100
     pygame.draw.rect(surface, CONSTAT.AZUL, (20, 50, largura, 30))
-    tela.blit(surface, (0, 200))
+    tela.blit(surface, (0, 300))
 
     # Total de memoria livre
     texto_total = "Total de Memória Livre: (" + str(round(mem_livre/(1024*1024*1024), 2))+" GB):"
     text = font.render(texto_total, 1, CONSTAT.BRANCO)
-    tela.blit(text, (20, 200))
+    tela.blit(text, (20, 300))
